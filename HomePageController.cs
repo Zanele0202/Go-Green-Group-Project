cb461b7cb9ebb1ab318752a9551cf40c476b5935
Zanele0202 <62016027+Zanele0202@users.noreply.github.com>
2020-10-13 18:49:50 +0200

    Add files via upload

---
+++ HomePageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Go_Green_Group_Project.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: HomePage
+        public ActionResult GoGreen()
+        {
+            return View();
+        }
+    }
+}
---
+++ RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using Go_Green_Group_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Go_Green_Group_Project.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Registration()
+        {
+           return View();
+        }
+    }
+}
